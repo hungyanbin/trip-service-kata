6d64b9272ecc831ee482e438e51b805a6506876a
yanbinHung <hung_yanbin@hotmail.com>
2020-03-04 01:11:10 +0800

    Add test case for not logged in user

--- src/test/java/org/craftedsw/tripservicekata/trip/TripServiceTest.java
+++ src/test/java/org/craftedsw/tripservicekata/trip/TripServiceTest.java
@@ -1,7 +1,25 @@
 package org.craftedsw.tripservicekata.trip;
 
+import org.craftedsw.tripservicekata.exception.UserNotLoggedInException;
+import org.craftedsw.tripservicekata.user.User;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class TripServiceTest {
+
+    @Test
+    void shouldThrowException_whenUserIsNotLoggedIn() {
+        TripService tripService = new NotLoggedInTripService();
+        User user = new User();
+
+        Assertions.assertThrows(UserNotLoggedInException.class,
+                () -> tripService.getTripsByUser(user));
+    }
+
+    private static class NotLoggedInTripService extends TripService {
+        @Override
+        protected User getLoggedUser() {
+            return null;
+        }
+    }
 }
